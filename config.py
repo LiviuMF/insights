981fc1f454cc7e30c7b4be891e11b3b34d23ab22
liviumf <liviu.m.farcas@gmail.com>
2025-06-16 11:17:02 +0300

    only need api url secret

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-API_USERNAME = os.environ.get('API_USERNAME')
-API_PASSWORD = os.environ.get('API_PASSWORD')
 API_URL = os.environ.get('API_URL')
