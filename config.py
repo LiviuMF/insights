f0377373eb88ff063cf5928225c1fb6b4d43386c
liviumf <liviu.m.farcas@gmail.com>
2025-10-21 22:52:37 +0300

    typo

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 API_URL = os.environ.get('API_URL')
 TEMP_REASONS = ['fara abatere', 'usa deschisa', 'defectiune tehnica', 'pana curent']
-TEMP_ACTIONS = ['fara actiune', 'usa inchsa', 'defectiune remediata', 'platit factura']
+TEMP_ACTIONS = ['fara actiune', 'usa inchisa', 'defectiune remediata', 'platit factura']
 
 COOLING_MAPPER = {
     'refrigerare (carne / organe)': [0,3],
