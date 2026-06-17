4df11116dcf8305d0dc935aee42ca2ed2dfe2eb6
liviumf <liviu.m.farcas@gmail.com>
2026-06-17 10:17:01 +0300

    Fix no devices for a certain user

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,14 @@ load_authentication_module()
 
 if st.session_state['is_authenticated']:
     api.fetch_devices()
-    fe.load_sidebar()
-    fe.load_device_notification_change()
-    utils.load_device_data()
-    api.load_readings_for_date()
-    api.fetch_owner_data()
-    load_general_css()
-    fe.load_haccp_report()
-    fe.load_temp_interval_change()
+    if 'selected_device' not in st.session_state:
+        st.sidebar.warning('Nu exista deviceuri pentru acest user')
+    else:
+        fe.load_sidebar()
+        fe.load_device_notification_change()
+        utils.load_device_data()
+        api.load_readings_for_date()
+        api.fetch_owner_data()
+        load_general_css()
+        fe.load_haccp_report()
+        fe.load_temp_interval_change()
